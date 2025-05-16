b68cc2c3b608ac9a9260226029ce88dcbd600785
Ruby <roobydemongirl@gmail.com>
2025-05-16 17:11:14 -0400

    v0.6

---
+++ Patches/ContinueGamePatch.cs
@@ -0,0 +1,61 @@
+﻿using JoelG.ENA4;
+using System.Reflection;
+using System;
+using UnityEngine;
+using HarmonyLib;
+
+namespace KatieSaveHelper.Patches
+{
+
+    // If performed by the mod, create a custom scene changer object for continuing the save
+
+    [HarmonyPatch(typeof(SaveFileData), "ContinueGame")]
+    public class ContinueGamePatch
+    {
+        private static readonly FieldInfo gameStateField = AccessTools.Field(typeof(SaveFileData), "gameState");
+        private static readonly FieldInfo sessionTraversalHistoryField = AccessTools.Field(typeof(SaveFileData), "sessionTraversalHistory");
+        private static readonly FieldInfo transitionField = AccessTools.Field(typeof(SceneChanger), "transition");
+
+        public static bool Prefix(SaveFileData __instance, bool resetState = true, float fadeInTime = 1f, float fadeOutTime = 1f)
+        {
+            SaveDataPlayerGameState gameState = (SaveDataPlayerGameState)gameStateField.GetValue(__instance);
+            SaveDataNodeTraversalHistory sessionTraversalHistory = (SaveDataNodeTraversalHistory)sessionTraversalHistoryField.GetValue(__instance);
+            SceneChanger sceneChanger;
+
+            if (gameState.HasCompletedGame)
+            {
+                new SceneChanger("Menu", Color.black, fadeInTime, fadeOutTime).GoToDestination();
+                return false;
+            }
+
+            if (resetState)
+            {
+                sessionTraversalHistory.ResetAllNodes();
+            }
+
+            if (!KatieSaveHelperMod.forceCustomTransition)
+            {
+                sceneChanger = new SceneChanger("Outworld", Color.black, fadeInTime, fadeOutTime);
+            }
+            else
+            {
+                sceneChanger = new SceneChanger("Outworld", KatieSaveHelperMod.customTransitionColor, KatieSaveHelperMod.customTransitionFadeInTime, KatieSaveHelperMod.customTransitionFadeOutTime);
+                transitionField.SetValue(sceneChanger, KatieSaveHelperMod.customTransitionType);
+            }
+
+            if (gameState.HasSavedSceneEntry)
+            {
+                string text = gameState.GetDestinationScene();
+                if (text.Equals("menu", StringComparison.OrdinalIgnoreCase))
+                {
+                    text = "Hub";
+                }
+
+                sceneChanger.SetDestination(text, gameState.SavedSceneEntrance ?? string.Empty);
+            }
+
+            sceneChanger.GoToDestination();
+            return false;
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,9 +1,9 @@
 ﻿using MelonLoader;
 using UnityEngine;
 using JoelG.ENA4;
-using KatieSaveHelper.Patches;
+using UnityEngine.SceneManagement;
 
-[assembly: MelonInfo(typeof(KatieSaveHelper.KatieSaveHelperMod), "Katie Save Helper", "1.0.5", "Katelyndev0211 and Zieraell")]
+[assembly: MelonInfo(typeof(KatieSaveHelper.KatieSaveHelperMod), "Katie Save Helper", "1.0.6", "Katelyndev0211 and Zieraell")]
 
 namespace KatieSaveHelper
 {
@@ -11,106 +11,119 @@ namespace KatieSaveHelper
     {
         private const string modGUID = "Zieraell.KatieSaveHelper";
         private const string modName = "Katie Save Helper";
-        private const string modVersion = "1.0.5.0";
+        private const string modVersion = "1.0.6.0";
         private const string modAuthors = "Katelyndev0211 and Zieraell";
 
-        internal static MelonPreferences_Category configCategory;
-        internal static MelonPreferences_Entry<KeyCode> quickSaveKeyConfig;
-        internal static MelonPreferences_Entry<KeyCode> reloadSaveKeyConfig;
-        internal static MelonPreferences_Entry<KeyCode> resetSaveKeyConfig;
-        internal static MelonPreferences_Entry<KeyCode> resetSaveWithSeedKeyConfig;
-        internal static MelonPreferences_Entry<KeyCode> reloadConfigKeyConfig;
-
-        internal static KeyCode quickSaveKey;
-        internal static KeyCode reloadSaveKey;
-        internal static KeyCode resetSaveKey;
-        internal static KeyCode resetSaveWithSeedKey;
-        internal static KeyCode reloadConfigKey;
+        internal static SceneChanger.TransitionType customTransitionType = SceneChanger.TransitionType.FadeToColor;
+        internal static bool forceCustomSeed = false;
+        internal static bool forceCustomTransition = false;
+        internal static int customSeed;
+        internal static Color customTransitionColor = Color.black;
+        internal static float customTransitionFadeInTime = 0;
+        internal static float customTransitionFadeOutTime = 0;
+        internal static bool allowSave = false;
 
         private readonly HarmonyLib.Harmony harmony = new HarmonyLib.Harmony(modGUID);
 
-        internal static bool forceCustomSeed;
-        internal static int customSeed;
-
-        void LoadConfig()
-        {
-            MelonLogger.Msg("Loading config...");
-
-            configCategory = MelonPreferences.CreateCategory("KatieSaveHelper", "Katie Save Helper Settings");
-
-            quickSaveKeyConfig = configCategory.CreateEntry("QuickSaveKey", KeyCode.Alpha1, "Key used to save the game");
-            reloadSaveKeyConfig = configCategory.CreateEntry("ReloadSaveKey", KeyCode.Alpha2, "Key used to reload the current save");
-            resetSaveKeyConfig = configCategory.CreateEntry("HardResetKey", KeyCode.Alpha0, "Key used to hard reset the current save");
-            resetSaveWithSeedKeyConfig = configCategory.CreateEntry("HardResetWithSeedKey", KeyCode.Alpha9, "Key used to hard reset the current save while keeping the same seed");
-            reloadConfigKeyConfig = configCategory.CreateEntry("ReloadConfigKey", KeyCode.Alpha8, "Key used to hard reset the current save while keeping the same seed");
-
-            configCategory.SaveToFile();
-
-            LoadKeysFromConfig();
-        }
-        void LoadKeysFromConfig()
-        {
-            quickSaveKey = quickSaveKeyConfig.Value;
-            reloadSaveKey = reloadSaveKeyConfig.Value;
-            resetSaveKey = resetSaveKeyConfig.Value;
-            resetSaveWithSeedKey = resetSaveWithSeedKeyConfig.Value;
-            reloadConfigKey = reloadConfigKeyConfig.Value;
-
-            MelonLogger.Msg(
-                $"Using keys: \n" +
-                $"\tSave={quickSaveKey}\n" +
-                $"\tReload={reloadSaveKey}\n" +
-                $"\tReset={resetSaveKey}\n" +
-                $"\tResetWithSeed={resetSaveWithSeedKey}\n" +
-                $"\tReloadConfig={reloadConfigKey}");
-        }
-
         public override void OnInitializeMelon()
         {
-            LoadConfig();
+            KatieSaveHelperModConfig.LoadConfig();
 
             MelonLogger.Msg($"{modName} loaded.");
             MelonLogger.Msg($"Mod by {modAuthors}");
 
-            harmony.PatchAll(typeof(SoftResetPatch));
+            harmony.PatchAll();
         }
 
         public override void OnUpdate()
         {
-            if (Input.GetKeyDown(quickSaveKey))
+            if (!Input.anyKeyDown)
+            {
+                return;
+            }
+
+            if (Input.GetKeyDown(KatieSaveHelperModConfig.quickSave_Key))
             {
                 MelonLogger.Msg("'Quick Save' key pressed");
-                SaveFile.WriteSave();
+                string currentSceneName = SceneManager.GetActiveScene().name;
+
+                if (currentSceneName == "Menu")
+                {
+                    MelonLogger.Msg("Cannot save in Main Menu");
+                    return;
+                }
+
+                allowSave = true;
+                if (currentSceneName == SaveFile.CurrentSave.GameState.GetDestinationScene())
+                {
+                    SaveFile.WriteSaveWithEntrance(SaveFile.CurrentSave.GameState.SavedSceneEntrance);
+                    MelonLogger.Msg("Save complete");
+                }
+                else
+                {
+                    SaveFile.WriteSaveWithEntrance();
+                    MelonLogger.Msg("Save complete (Failsafe)");
+                }
+                allowSave = false;
+                return;
             }
 
-            if (Input.GetKeyDown(reloadSaveKey))
+            if (Input.GetKeyDown(KatieSaveHelperModConfig.reloadSave_Key))
             {
                 MelonLogger.Msg("'Reload Save' key pressed");
+
+                forceCustomTransition = true;
+                customTransitionType = KatieSaveHelperModConfig.reloadSave_TransitionType;
+                customTransitionColor = KatieSaveHelperModConfig.reloadSave_TransitionColor;
+                customTransitionFadeInTime = KatieSaveHelperModConfig.reloadSave_TransitionFadeInTime;
+                customTransitionFadeOutTime = KatieSaveHelperModConfig.reloadSave_TransitionFadeOutTime;
+
                 SaveFile.ContinueSave();
+                forceCustomTransition = false;
+                return;
             }
 
-            if (Input.GetKeyDown(resetSaveKey))
+            if (Input.GetKeyDown(KatieSaveHelperModConfig.resetSave_Key))
             {
                 MelonLogger.Msg("'Reset Save' key pressed");
+
+                forceCustomTransition = true;
+                customTransitionType = KatieSaveHelperModConfig.resetSave_TransitionType;
+                customTransitionColor = KatieSaveHelperModConfig.resetSave_TransitionColor;
+                customTransitionFadeInTime = KatieSaveHelperModConfig.resetSave_TransitionFadeInTime;
+                customTransitionFadeOutTime = KatieSaveHelperModConfig.resetSave_TransitionFadeOutTime;
+
                 SaveFile.ResetSave(MetaSaveFile.Current.SaveIndex);
                 SaveFile.ContinueSave();
+                forceCustomTransition = false;
+                return;
             }
 
-            if (Input.GetKeyDown(resetSaveWithSeedKey))
+            if (Input.GetKeyDown(KatieSaveHelperModConfig.resetSaveWithSeed_Key))
             {
                 MelonLogger.Msg("'Reset Save with Seed' key pressed");
+
+                forceCustomTransition = true;
+                customTransitionType = KatieSaveHelperModConfig.resetSaveWithSeed_TransitionType;
+                customTransitionColor = KatieSaveHelperModConfig.resetSaveWithSeed_TransitionColor;
+                customTransitionFadeInTime = KatieSaveHelperModConfig.resetSaveWithSeed_TransitionFadeInTime;
+                customTransitionFadeOutTime = KatieSaveHelperModConfig.resetSaveWithSeed_TransitionFadeOutTime;
+
                 forceCustomSeed = true;
                 customSeed = SaveFile.CurrentSave.SaveHash;
                 SaveFile.ResetSave(MetaSaveFile.Current.SaveIndex);
                 SaveFile.ContinueSave();
                 forceCustomSeed = false;
+                forceCustomTransition = false;
+                return;
             }
 
-            if (Input.GetKeyDown(reloadConfigKey))
+            if (Input.GetKeyDown(KatieSaveHelperModConfig.reloadConfig_Key))
             {
                 MelonLogger.Msg("'Reload Config' key pressed");
                 MelonPreferences.Load();
-                LoadKeysFromConfig();
+                KatieSaveHelperModConfig.LoadOptionsFromConfig();
+                return;
             }
         }
     }
